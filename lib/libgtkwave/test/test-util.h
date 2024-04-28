0f1be4735b1161f36bcc8cac934636038d1abdcb
Ralf Fuest <mail@rfuest.de>
2024-04-28 18:15:21 +0200

    Remove old loader tests

--- lib/libgtkwave/test/test-util.h
+++ lib/libgtkwave/test/test-util.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "gw-tree.h"
-#include "gw-dump-file.h"
 
 void assert_tree(GwTreeNode *node, const gchar *expected);
 GwTreeNode *get_tree_node(GwTree *tree, const gchar *path);
-void common_basic_vcd_and_fst_test(GwDumpFile *file);
