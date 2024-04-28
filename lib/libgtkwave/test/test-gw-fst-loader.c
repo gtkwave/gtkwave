426e24180710539c755fc080d1574d9fc843c6b2
Ralf Fuest <mail@rfuest.de>
2024-04-28 18:15:21 +0200

    Remove old loader tests

--- lib/libgtkwave/test/test-gw-fst-loader.c
+++ lib/libgtkwave/test/test-gw-fst-loader.c
@@ -1,19 +1,4 @@
 #include <gtkwave.h>
-#include "test-util.h"
-
-static void test_basic()
-{
-    GwLoader *loader = gw_fst_loader_new();
-
-    GError *error = NULL;
-    GwDumpFile *file = gw_loader_load(loader, "files/basic.fst", &error);
-    g_assert_no_error(error);
-    g_object_unref(loader);
-
-    common_basic_vcd_and_fst_test(file);
-
-    g_object_unref(file);
-}
 
 static void test_enum()
 {
@@ -58,7 +43,6 @@ int main(int argc, char *argv[])
 {
     g_test_init(&argc, &argv, NULL);
 
-    g_test_add_func("/fst_loader/basic", test_basic);
     g_test_add_func("/fst_loader/enum", test_enum);
     g_test_add_func("/fst_loader/error_file_not_found", test_error_file_not_found);
 
--- lib/libgtkwave/test/test-gw-vcd-loader.c
+++ lib/libgtkwave/test/test-gw-vcd-loader.c
@@ -1,19 +1,4 @@
 #include <gtkwave.h>
-#include "test-util.h"
-
-static void test_basic()
-{
-    GwLoader *loader = gw_vcd_loader_new();
-
-    GError *error = NULL;
-    GwDumpFile *file = gw_loader_load(loader, "files/basic.vcd", &error);
-    g_assert_no_error(error);
-    g_object_unref(loader);
-
-    common_basic_vcd_and_fst_test(file);
-
-    g_object_unref(file);
-}
 
 static void test_error_file_not_found()
 {
@@ -31,7 +16,6 @@ int main(int argc, char *argv[])
 {
     g_test_init(&argc, &argv, NULL);
 
-    g_test_add_func("/vcd_loader/basic", test_basic);
     g_test_add_func("/vcd_loader/error_file_not_found", test_error_file_not_found);
 
     return g_test_run();
