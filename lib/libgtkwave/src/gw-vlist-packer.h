d84f3e7c41b90ae4722f21563fcc70908a3c9244
Ralf Fuest <mail@rfuest.de>
2024-05-05 20:44:43 +0200

    Add GwVlistReader

--- lib/libgtkwave/src/gw-vlist-packer.h
+++ lib/libgtkwave/src/gw-vlist-packer.h
@@ -10,4 +10,4 @@ void gw_vlist_packer_alloc(GwVlistPacker *self, unsigned char ch);
 GwVlist *gw_vlist_packer_finalize_and_free(GwVlistPacker *self);
 
 unsigned char *gw_vlist_packer_decompress(GwVlist *vl, unsigned int *declen);
-void gw_vlist_packer_decompress_destroy(char *mem);
+void gw_vlist_packer_decompress_destroy(guchar *mem);
