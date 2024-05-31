0b35ad384eaeef136f972a156911888672c7dfcd
Ralf Fuest <mail@rfuest.de>
2024-05-31 18:09:05 +0200

    Replace VZT flags in FST loader with dedicated type

--- lib/libgtkwave/src/gw-fac.h
+++ lib/libgtkwave/src/gw-fac.h
@@ -18,3 +18,12 @@ struct _GwFac
 #ifdef WAVE_USE_STRUCT_PACKING
 #pragma pack(pop)
 #endif
+
+typedef enum {
+    GW_FAC_FLAG_BITS = 0,
+    GW_FAC_FLAG_INTEGER = 1 << 0,
+    GW_FAC_FLAG_DOUBLE = 1 << 1,
+    GW_FAC_FLAG_STRING = 1 << 2,
+    GW_FAC_FLAG_ALIAS = 1 << 3,
+    GW_FAC_FLAG_SYNVEC = 1 << 17,
+} GwFacFlags;
