773d8dc00bb6b5dc08c2f0c20ff2173877537d21
Ralf Fuest <mail@rfuest.de>
2024-06-10 18:13:03 +0200

    Remove unused functions makevec_range, makevec_selected, and add_vector_range

--- src/symbol.h
+++ src/symbol.h
@@ -44,9 +44,6 @@ int maketraces(char *, char *, int);
 
 /* additions to bitvec.c because of search.c/menu.c ==> formerly in analyzer.h */
 GwBitVector *bits2vector(GwBits *b);
-GwBits *makevec_selected(char *vec, int numrows, char direction);
-GwBits *makevec_range(char *vec, int lo, int hi, char direction);
-int add_vector_range(char *alias, int lo, int hi, char direction);
 GwBits *makevec_chain(char *vec, GwSymbol *sym, int len);
 int add_vector_chain(GwSymbol *s, int len);
 char *makename_chain(GwSymbol *sym);
