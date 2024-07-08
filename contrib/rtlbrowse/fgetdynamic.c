f5babef5def5c9a848c1d5286243d44791fa9682
CoA <1109673069@qq.com>
2024-07-08 23:47:48 +0800

    rtlbrowse: Replace alloc_bytechain with GString
    This is identical to 4495e3

--- contrib/rtlbrowse/fgetdynamic.c
+++ contrib/rtlbrowse/fgetdynamic.c
@@ -7,69 +7,37 @@
  * of the License, or (at your option) any later version.
  */
 
-#ifdef _AIX
-#pragma alloca
-#endif
-
-#include <config.h>
 #include "fgetdynamic.h"
 #include <stdlib.h>
-
-#ifdef HAVE_ALLOCA_H
-#include <alloca.h>
-#elif defined(__GNUC__)
-#ifndef alloca
-#define alloca __builtin_alloca
-#endif
-#endif
 
 int fgetmalloc_len;
 
 char *fgetmalloc(FILE *handle)
 {
-char *pnt, *pnt2;
-struct alloc_bytechain *bytechain_root=NULL, *bytechain_current=NULL;
-int ch;
+    GString *line = g_string_new(NULL);
 
-fgetmalloc_len=0;
+    for (;;) {
+        int ch = fgetc(handle);
+        if (ch == EOF || ch == 0x00 || ch == '\n' || ch == '\r') {
+            if (ch > 0 && line->len == 0) {
+                continue; // skip leading CRs and LFs
+            } else {
+                break;
+            }
+        }
 
-for(;;)
-	{
-	ch=fgetc(handle);
-	if((ch==EOF)||(ch==0x00)||(ch=='\n')||(ch=='\r')) break;
-	fgetmalloc_len++;
-	if(bytechain_current)
-		{
-		bytechain_current->next=alloca(sizeof(struct alloc_bytechain));
-		bytechain_current=bytechain_current->next;
-		bytechain_current->val=(char)ch;
-		bytechain_current->next=NULL;
-		}
-		else
-		{
-		bytechain_root=bytechain_current=alloca(sizeof(struct alloc_bytechain));
-		bytechain_current->val=(char)ch;
-		bytechain_current->next=NULL;
-		}
-	}
+        g_string_append_c(line, ch);
+    }
 
-if(!fgetmalloc_len)
-	{
-	return(NULL);
-	}
-	else
-	{
-	pnt=pnt2=(char *)malloc(fgetmalloc_len+1);
-	while(bytechain_root)
-		{
-		*(pnt2++)=bytechain_root->val;
-		bytechain_root=bytechain_root->next;
-		}
-	*(pnt2)=0;
+    fgetmalloc_len = line->len;
 
-	return(pnt);
-	}
+    char *ret = NULL;
+    if (fgetmalloc_len > 0) {
+        ret = malloc(fgetmalloc_len + 1);
+        memcpy(ret, line->str, fgetmalloc_len + 1);
+    }
 
+    g_string_free(line, TRUE);
 
+    return ret;
 }
-
