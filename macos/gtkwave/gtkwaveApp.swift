b8a43345e49027965d90be249457c7745b2c09f3
Qiming Chu <cchuqiming@gmail.com>
2025-01-10 10:02:25 +0800

    feat: Add macOS application wrapper in gtkwave
    The benefits of this change include:
    
    1. Improved user experience for macOS users, as they can now launch
    gktwave directly from the Applications folder like any other native
    macOS app.
    2. Easier distribution and installation of gktwave on macOS systems, as
    users can simply download and drag the application bundle to their
    Applications folder.

---
+++ macos/gtkwave/gtkwaveApp.swift
@@ -0,0 +1,54 @@
+//
+//  macosApp.swift
+//  macos
+//
+//  Created by Emin017 on 2025/1/9.
+//
+
+import Cocoa
+
+@main
+class macOSApp {
+    static func main() {
+        let task = Process()
+        let bundle = Bundle.main
+
+        if let resourceURL = bundle.resourceURL?.appendingPathComponent("bin/gtkwave") {
+            task.executableURL = resourceURL
+        } else {
+            fatalError("Binary 'gtkwave' not found in Resources/app folder")
+        }
+
+        let stdOut = Pipe()
+        let stdErr = Pipe()
+
+        stdOut.fileHandleForReading.readabilityHandler = { fileHandle in
+            log(fileHandle.availableData)
+        }
+
+        stdErr.fileHandleForReading.readabilityHandler = { fileHandle in
+            log(fileHandle.availableData)
+        }
+
+        task.standardOutput = stdOut
+        task.standardError = stdErr
+
+        task.terminationHandler = { task in
+            (task.standardOutput as AnyObject?)?.fileHandleForReading.readabilityHandler = nil
+            (task.standardError as AnyObject?)?.fileHandleForReading.readabilityHandler = nil
+        }
+
+        do {
+            try task.run() // Start task
+            task.waitUntilExit() // Keep syncing, until task end
+        } catch {
+            NSLog("Failed to start task: \(error)")
+        }
+    }
+    // Log function
+    static func log(_ data: Data) {
+        if let message = String(data: data, encoding: .utf8) {
+            NSLog("%@", message)
+        }
+    }
+}
