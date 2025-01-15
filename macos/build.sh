452bce2b934788e3abe9f8a62b59b6a3abc0eacb
Qiming Chu <cchuqiming@gmail.com>
2025-01-15 14:20:28 +0800

    rework: Add build scripts for building macos app bundle

---
+++ macos/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+meson setup "$DIR/build" \
+    -Dbuild_macos_app=enabled \
+    -Dset_rpath=enabled \
+    -Dintrospection=false \
+    --prefix=/Applications/GTKWave.app \
+    --datadir=/Applications/GTKWave.app/Contents/Resources/share \
+    --bindir=Contents/Resources/bin --libdir=Contents/Resources/lib
+
+meson install -C "$DIR/build"
