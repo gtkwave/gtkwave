5717b34bc21b5347640e21ccb62a577e94c1d86b
Qiming Chu <cchuqiming@gmail.com>
2025-01-15 21:04:58 +0800

    GTKWave.app: Add install_deps.sh for adding runtime deps in bundle

--- macos/launcher.sh
+++ macos/launcher.sh
@@ -4,4 +4,6 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 CONTENTS="$(dirname "$DIR")"
 RESOURCES="$CONTENTS/Resources"
 
+export DYLD_LIBRARY_PATH="$RESOURCES/lib:${DYLD_LIBRARY_PATH:-}"
+
 exec "$RESOURCES/bin/gtkwave" "$@"
