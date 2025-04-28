6cd3033b1dc1e6feba984a2b24803b1424b05676
gtkwave <bybell@rocketmail.com>
2025-04-28 00:38:38 -0400

    Prepare for next development version cycle.

--- gtkwave3/src/version.h
+++ gtkwave3/src/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Tony Bybell 2010-2024.
+ * Copyright (c) Tony Bybell 2010-2025.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -10,6 +10,6 @@
 #ifndef WAVE_VERSION_H
 #define WAVE_VERSION_H
 
-#define WAVE_VERSION_INFO "GTKWave Analyzer v" PACKAGE_VERSION " (w)1999-2024 BSI"
+#define WAVE_VERSION_INFO "GTKWave Analyzer v" PACKAGE_VERSION " (w)1999-2025 BSI"
 
 #endif
