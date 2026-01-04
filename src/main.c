af3e49a764d1c9ea1be128535226664e0f3b5679
Natalia <nataliakokoromyti@gmail.com>
2026-01-04 12:49:37 -0800

    restore x11 guard

--- src/main.c
+++ src/main.c
@@ -23,8 +23,9 @@
 #ifdef __MINGW32__
 #include <windows.h>
 #else
-#if GTK_CHECK_VERSION(3, 0, 0)
+#ifdef GDK_WINDOWING_X11
 #include <gtk/gtkx.h>
+#include <gdk/gdkx.h>
 #endif
 #endif
 
@@ -1262,6 +1263,24 @@ do_primary_inits:
             }
         }
     }
+
+#ifdef WAVE_USE_XID
+    if (GLOBALS->socket_xid) {
+        gboolean is_x11_display = FALSE;
+#ifdef GDK_WINDOWING_X11
+        {
+            GdkDisplay *display = gdk_display_get_default();
+            if (GDK_IS_X11_DISPLAY(display)) {
+                is_x11_display = TRUE;
+            }
+        }
+#endif
+        if (!is_x11_display) {
+            fprintf(stderr, "GTKWAVE | Ignoring -X; GtkPlug only works on X11.\n");
+            GLOBALS->socket_xid = 0;
+        }
+    }
+#endif
 
 #ifdef HAVE_WCP
     if (wcp_initiate_address) {
