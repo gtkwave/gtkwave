16063566bbf2a8a3891d0262f4a8d3a84d06ef9c
Zapta <zapta@users.noreply.github.com>
2026-02-21 10:47:37 -0800

    Removed the XPM warning filter, it's not needed anymore.

--- macos/launcher.sh
+++ macos/launcher.sh
@@ -8,8 +8,4 @@ RESOURCES="$CONTENTS/Resources"
 
 export DYLD_LIBRARY_PATH="$RESOURCES/lib:${DYLD_LIBRARY_PATH:-}"
 
-# TODO: Find a cleaner solution to avoid the warnings.
-# For now we filter out these warnings and empty lines that
-# tend to appear between them.
-
-exec "$RESOURCES/bin/gtkwave" "$@" 2>&1 | grep -v -E '^$|GdkPixbuf-WARNING.*Error loading XPM image loader'
+exec "$RESOURCES/bin/gtkwave" "$@"
