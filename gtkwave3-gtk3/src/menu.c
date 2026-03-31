7c1e8255753a90abcb546133ca3fd2161da0b4c9
gtkwave <bybell@rocketmail.com>
2026-03-31 01:17:38 -0400

    Fix use after free of GLOBALS->entrybox_text in regexp_highlight_generic().

--- gtkwave3-gtk3/src/menu.c
+++ gtkwave3-gtk3/src/menu.c
@@ -4540,7 +4540,7 @@ if(GLOBALS->entrybox_text)
 
 	strcpy(GLOBALS->regexp_string_menu_c_1, GLOBALS->entrybox_text);
 	wave_regex_compile(GLOBALS->regexp_string_menu_c_1, WAVE_REGEX_SEARCH);
-	free_2(GLOBALS->entrybox_text);
+	free_2(GLOBALS->entrybox_text); GLOBALS->entrybox_text = NULL;
 	t=GLOBALS->traces.first;
 	while(t)
 		{
